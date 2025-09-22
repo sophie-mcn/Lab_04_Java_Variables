bc00e71929b936c05eab1a269f9ddbcc5578f25a
sophie-mcn <mcnamask@mail.uc.edu>
2025-09-22 08:15:22 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,48 @@
 public class Main {
     public static void main(String[] args) {
 
+        int intOperandA = 9;
+        int intOperandB = 32;
 
-        
+        int intSum =  intOperandA + intOperandB;
+        System.out.println("The sum of " +  intOperandA + " and " + intOperandB + " is " + intSum);
+
+        int intProduct =  intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        int intDifference = intOperandB - intOperandA;
+        System.out.println("The difference between " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        int intQuotient = intOperandB / intOperandA;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        int intModulo = intOperandB % intOperandA;
+        System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+        double doubleOperandA = 4.1;
+        double doubleOperandB = 12.2;
+
+        double doubleSum =  doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        double doubleProduct =  doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        double doubleDifference = doubleOperandB - doubleOperandA;
+        System.out.println("The difference between " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        double doubleQuotient = doubleOperandB / doubleOperandA;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+
+        int familyKids =
+        boolean isRaining =
+        double gasCost =
+        int favNumber =
+        double shoeSize =
+        String birthMonth =
+        String fullName =
+
     }
 }
